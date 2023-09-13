65a877dfdbe8d5bae64b20ebdff77174d29dc744
Luca Azalim <lucaazalim@gmail.com>
2023-09-13 11:54:39 -0300

    Implementando classe Client e ClientTest.

--- codigo/src/modules/Client.java
+++ codigo/src/modules/Client.java
@@ -1,5 +1,45 @@
 package modules;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
 public class Client {
+
+    private static int counter = 0;
+
+    private final int id;
+
+    private final String name;
+
+    private List<Rent> rents;
+
+    public Client(String name) {
+
+        Objects.requireNonNull(name);
+
+        this.id = counter++;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Rent> getRents() {
+        return rents;
+    }
+
+    public void rentEquipment(Rent rent) {
+        this.rents.add(rent);
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
     
 }
---
+++ codigo/src/tests/ClientTest.java
@@ -0,0 +1,54 @@
+package tests;
+
+import modules.Client;
+import modules.Equipment;
+import modules.Rent;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.function.Executable;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ClientTest {
+
+    private static Client client;
+
+    @Test
+    @Order(1)
+    public void testClientCreation() {
+
+        assertDoesNotThrow(() -> {
+            client = new Client("Luca Ferrari Azalim");
+        }, "Client creation should not throw an exception");
+
+        assertThrows(NullPointerException.class, () -> {
+            client = new Client(null);
+        }, "Client creation should throw a NullPointerException");
+
+        assertEquals("Luca Ferrari Azalim", client.getName(), "Client name should be Luca Ferrari Azalim");
+        assertEquals(0, client.getRents().size(), "Client rents should be empty");
+
+    }
+
+    @Test
+    public void testClientCounter() {
+
+        assertEquals(1, Client.getCounter(), "Client counter should be 0");
+
+    }
+
+    @Test
+    public void testClientRentEquipment() {
+
+        Equipment equipment = new Equipment();
+        Rent rent = new Rent(LocalDate.now().plusDays(10), client, equipment);
+
+        assertDoesNotThrow(() -> client.rentEquipment(rent), "Client rent equipment should not throw an exception");
+
+        assertEquals(1, client.getRents().size(), "Client rents should have 1 rent");
+
+    }
+
+}
